a272a5c81342e889f608647f9ac815e2dbefb3c8
OrlandoJ15 <orlando.org.1994@gmail.com>
2024-11-29 14:06:06 -0600

    1
    1

--- MetodosComunes/obj/Release/net8.0/MetodosComunes.AssemblyInfo.cs
+++ MetodosComunes/obj/Release/net8.0/MetodosComunes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MetodosComunes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e3f0a68d738ef984f6948e4029e0af5141cb938")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+142cd5832d3122ff398b1e4b2ca098f9f6be571f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MetodosComunes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MetodosComunes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

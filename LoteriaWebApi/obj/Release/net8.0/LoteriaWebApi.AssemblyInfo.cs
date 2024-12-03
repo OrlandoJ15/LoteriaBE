46c2a6fedb33074c5d5115ba74c911049446ba79
OrlandoJ15 <orlando.org.1994@gmail.com>
2024-12-02 20:57:42 -0600

    s
    s

--- LoteriaWebApi/obj/Release/net8.0/LoteriaWebApi.AssemblyInfo.cs
+++ LoteriaWebApi/obj/Release/net8.0/LoteriaWebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoteriaWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+895a8d392170904931180aff3778395d63c034f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1106dc7c2cec5e549e846ab371ef90cdc902254")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoteriaWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoteriaWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
